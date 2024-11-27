611465eb1b714c2b999e3bff761bbffa3db42f58
Acipzz <moses.alexander@student.umn.ac.id>
2024-11-27 19:29:40 +0700

    hero

--- Manadong/src/components/Navbar.jsx
+++ Manadong/src/components/Navbar.jsx
@@ -1,34 +1,41 @@
-import MyLogo from "../assets/Manadong.svg";
+import React from 'react';
+import MyLogo from '../assets/Manadong.svg';
 
 const Navbar = () => {
-    return (
-      <nav className="bg-blue-900 p-4">
-        <div className="max-w-7xl mx-auto flex items-center justify-between">
-          {/* Logo */}
-          <div className="text-white font-bold text-lg">
-            <a href="/">
-                <img src={MyLogo} alt="Manadong" className="h-10" />
-            </a>
-          </div>
-  
-          {/* Menu Navigasi */}
-          <div className=" lg:flex space-x-4">
-            <a href="/" className="text-white hover:text-gray-300">
-              Home
-            </a>
-            <a href="/about" className="text-white hover:text-gray-300">
-              About
-            </a>
-            <a href="/services" className="text-white hover:text-gray-300">
-              Services
-            </a>
-            <a href="/contact" className="text-white hover:text-gray-300">
-              Contact
-            </a>
-          </div>
+  return (
+    <nav className="bg-blue-900 p-4">
+      <div className="max-w-7xl mx-auto flex items-center justify-between">
+        {/* Logo */}
+        <div className="text-white font-bold text-lg">
+          <a href="/">
+            <img src={MyLogo} alt="Manadong" className="h-10" />
+          </a>
         </div>
-      </nav>
-    );
-  };
-  
-  export default Navbar;
+
+        <div className="hidden lg:flex space-x-4">
+          <a href="/" className="text-white hover:text-gray-300">
+            Home
+          </a>
+          <a href="/about" className="text-white hover:text-gray-300">
+            About
+          </a>
+          <a href="/services" className="text-white hover:text-gray-300">
+            Services
+          </a>
+          <a href="/contact" className="text-white hover:text-gray-300">
+            Contact
+          </a>
+        </div>
+
+        {/* Mobile Menu Button */}
+        <button className="lg:hidden block">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6 text-white">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
